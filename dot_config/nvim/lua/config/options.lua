630d53c652a269ac490dce2962db7b205cab968d
Rémi Hillairet <contact@remihillairet.com>
2026-03-27 11:31:18 +0100

    feat(neovim): use omarchy config

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,8 +1,3 @@
-vim.g.snacks_animate = false
-vim.g.lazyvim_check_order = false
-
-vim.opt.ignorecase = true
-
-vim.opt.number = false
-vim.opt.relativenumber = false
-vim.opt.scrolloff = 8
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
--- dot_config/nvim/lua/plugins/disabled.lua
+++
@@ -1,5 +0,0 @@
-return {
-  { "nvim-mini/mini.pairs", enabled = false },
-  { "folke/noice.nvim",     enabled = false },
-  { "rcarriga/nvim-notify", enabled = false },
-}
